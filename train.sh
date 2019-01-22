5a3d9859c194e9d46342c610a07190e2313b496b
dingmingyu <dingmingyu@sensetime.com>
2019-01-23 00:09:53 +0800

    aaa

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-srun --mpi=pmi2 -p Segmentation -n1 --gres=gpu:8 --ntasks-per-node=1 python -u train.py --dataroot ./datasets/horse2zebra --name humen2cartoon_single --model cycle_gan --display_port 9829 --gpu_ids 0,1,2,3,4,5,6,7 --batch_size 128
+srun --mpi=pmi2 -p Segmentation -n1 --gres=gpu:8 --ntasks-per-node=1 python -u train.py --dataroot ./datasets/horse2zebra --name humen2cartoon_single_with_residual --model cycle_gan --display_port 9830 --gpu_ids 0,1,2,3,4,5,6,7 --batch_size 128
